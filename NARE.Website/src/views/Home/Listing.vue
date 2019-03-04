71081a95fab3a8ed0a4f5bdef0b10dad79e41e2a
reecerose <reecerose18@gmail.com>
2019-03-03 23:29:52 -0500

    Added modal carousel for listing images

--- NARE.Website/src/views/Home/Listing.vue
+++ NARE.Website/src/views/Home/Listing.vue
@@ -5,9 +5,31 @@
             <div class="col-lg-9 col-md-8 col-sm-12 pb-3">
                 <div class="property border-main">
                     <img class="img-fluid" :src="post.imageUrl" alt="Main picture">
-                    <div class="row">
-                        <div v-for="(picture, index) in post.pictures" :key="index" class="col-3 m-2 pr-0">
-                            <img :src="picture.source" :alt="picture.alternative" class="img-fluid rounded">
+                    <div class="row p-3">
+                        <div v-for="(picture, index) in post.pictures" :key="index" class="col-lg-3 col-md-4 col-sm-6 pb-4">
+                            <img :src="picture.src" :alt="picture.title" class="img-fluid rounded" @click="toggleModal(index)">
+                        </div>
+                        <div class="modal" v-if="displayModal">
+                            <div class="modal-header">
+                                <button class="close text-white" @click="displayModal = false">&times;</button>
+                            </div>
+                            <div class="modal-body text-center">
+                                <div class="carousel slide" data-interval="false" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item" v-for="(picture, pictureIndex) in post.pictures" :key="pictureIndex" :class="pictureIndex == currentImage ? 'active' : ''">                            
+                                            <img :src="picture.src" :alt="picture.title" class="image-modal">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" @click="previouseSlide">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" @click="nextSlide">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <p class="text-center address font-bold mb-2">{{ post.address }}</p>
@@ -26,12 +48,12 @@
                                 <div class="col text-right">{{ post.bedrooms }}</div>
                             </div>
                             <hr>
-                                <div class="row">
+                            <div class="row">
                                 <div class="col"><p><i class="fas fa-bath"></i> Bathrooms:</p></div>
                                 <div class="col text-right">{{ post.bathrooms }}</div>
                             </div>
                             <hr>
-                                <div class="row">
+                            <div class="row">
                                 <div class="col"><p><i class="fas fa-car"></i> Garage:</p></div>
                                 <div class="col text-right">{{ post.garage }}</div>
                             </div>
@@ -49,12 +71,12 @@
                                 <div class="col text-right">{{ post.lotSize }}</div>
                             </div>
                             <hr>
-                                <div class="row">
+                            <div class="row">
                                 <div class="col"><p><i class="fas fa-calendar-alt"></i> Listing Date:</p></div>
                                 <div class="col text-right">{{ post.listingDate }}</div>
                             </div>
                             <hr>
-                                <div class="row">
+                            <div class="row">
                                 <div class="col"><p><i class="fas fa-user"></i> Realtor:</p></div>
                                 <div class="col text-right">{{ post.agent.name }}</div>
                             </div>
@@ -84,10 +106,44 @@
 </template>
 
 <script>
+
 export default {
     name: 'listing',
+    methods: {
+        toggleModal(imageIndex) {
+            this.displayModal = !this.displayModal
+            this.currentImage = imageIndex
+        },
+        nextSlide() {
+            if (this.currentImage >= this.post.pictures.length - 1) {
+                this.currentImage = 0
+            } else {
+                this.currentImage++
+            }
+        },
+        previouseSlide() {
+            if (this.currentImage <= 0) {
+                this.currentImage = this.post.pictures.length - 1
+            } else {
+                this.currentImage--
+            }
+        }
+    },
+    mounted() {
+        document.body.addEventListener('keyup', e => {
+            if (e.keyCode === 27) {
+                this.displayModal = false
+            } else if (e.keyCode === 37) {
+                this.previouseSlide();
+            } else if (e.keyCode === 39) {
+                this.nextSlide()
+            }
+        })
+    },
     data() {
         return {
+            displayModal: false,
+            currentImage: 0,
             post: {
                     id: '1231',
                     address: '123 Test Ave',
@@ -109,17 +165,33 @@ export default {
                     },
                     pictures: [
                         {
-                            source: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                            alternative: 'alt'
+                            src: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                            title: 'Alt'
                         },
                         {
-                            source: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                            alternative: 'alt'
-                        }
+                            src: 'https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                            title: 'Alt'
+                        },
+                        {
+                            src: 'https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                            title: 'Alt'
+                        },
+                        {
+                            src: 'https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                            title: 'Alt'
+                        },
+                        {
+                            src: 'https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                            title: 'Alt'
+                        },
+                        {
+                            src: 'https://images.pexels.com/photos/276554/pexels-photo-276554.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                            title: 'Alt'
+                        },
                     ],
                     listingDate: '01/01/2019',
                     imageUrl: 'https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                },
+                }
         }
     }
 }
@@ -144,5 +216,26 @@ export default {
 }
 .address-minor {
     font-size: 0.75rem;
+}
+.modal {
+    display: block;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0,0,0,0.9);
+
+    .modal-header {
+        border: none;
+    }
+
+    .image-modal {
+        width: 75vw;
+        height: auto;
+    }
 }
 </style>
