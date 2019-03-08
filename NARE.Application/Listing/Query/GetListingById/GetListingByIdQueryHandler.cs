ea80ca99ee8ff2583616635dd57fefc9f49cc437
reecerose <reecerose18@gmail.com>
2019-03-07 21:34:47 -0500

    Added implementation of GetListingById

--- NARE.Application/Listing/Query/GetListingById/GetListingByIdQueryHandler.cs
+++ NARE.Application/Listing/Query/GetListingById/GetListingByIdQueryHandler.cs
@@ -1,14 +1,23 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NARE.Persistence;
 
 namespace NARE.Application.Listing.Query.GetListingById
 {
     public class GetListingByIdQueryHandler : IRequestHandler<GetListingByIdQuery, Domain.Entities.Listing>
     {
-        public Task<Domain.Entities.Listing> Handle(GetListingByIdQuery request, CancellationToken cancellationToken)
+        private readonly ApplicationDbContext _context;
+
+        public GetListingByIdQueryHandler(ApplicationDbContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<Domain.Entities.Listing> Handle(GetListingByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Listings.FirstOrDefaultAsync(l => l.Id == request.Id, cancellationToken: cancellationToken);
         }
     }
 }
